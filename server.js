c5e05809858544d60638be4f63c836a8c33a8b6e
drmaduski <ucheqavah@gmail.com>
2025-10-12 21:26:41 +0100

    modified token use and test for app

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/profile-picture-andrea', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-andrea.jpg"));
+  let img = fs.readFileSync(path.join(__dirname, "images/profile-uchenna.jpg"));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
