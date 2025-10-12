3a137fdca8dcdf7d4538f968e076949e96477419
drmaduski <ucheqavah@gmail.com>
2025-10-12 21:43:50 +0100

    adjusted .env

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('/profile-picture-andrea', function (req, res) {
+app.get('/profile-picture-uchenna', function (req, res) {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-uchenna.jpg"));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
